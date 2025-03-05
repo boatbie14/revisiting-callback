7aab355c85ba3e9147f6fec4b1c9d3d92bb6bad1
Kittiphat <kittiphat.rpp@gmail.com>
2025-03-05 10:18:20 +0700

    exercise: ex1-2 - ex2-2

--- th/ex2-2.js
+++ th/ex2-2.js
@@ -2,13 +2,27 @@
 1. Callback & Higher-Order Function มีข้อดีอย่างไร
 Answer:
 
+Code - สะอาด, Maintain ง่าย 
+
 
 2. จากการลองใช้ Callback Function ในระบบเกมและระบบแจ้งเตือน คุณคิดว่าโค้ดอ่านง่ายขึ้นหรือไม่ เพราะอะไร 
 Answer:
 
+ง่ายขึ่นไม่ต้องใส่ If - else ให่้วุ่นวาย 
 
-3. หากต้องสอนเพื่อนให้เข้าใจ allback & Higher-Order Function ใน 5 นาที จะอธิบายอย่างไร 
+
+3. หากต้องสอนเพื่อนให้เข้าใจ Callback & Higher-Order Function ใน 5 นาที จะอธิบายอย่างไร 
 Answer:  
+
+- Callback Function คือ Function ที่ถูกส่งเป็น Parameter ไปยัง Function อื่น 
+- Higher-Order Function คือ Function ที่เอา Function อื่น ๆ มาเป็น Parameter และเป็นตัวรับ Return จาก Parameter อื่น ๆ มาใช้งาน
+
+ยกตัวอย่าง:
+
+ระบบแจ้งเตือนบน ที่ทำงานต่างกันไปตามเหตุการณ์ เช่น โพสต์ใหม่, มีคนกดไลค์, มีคนคอมเมนต์ หรือมีการแชร์โพสต์ โดยมี Higher-Order Function เป็นโรงงาน ที่คอยเรียกใช้งาน Callback Function ตามความต้องการ
+
+ข้อดีหากคุณต้อง Maintain หรือ ต้องเขียนระบบ แจ้งเตือนคนกด Link ใหม่ คุณก็ไม่ต้องไปยุ่งกับ ส่วนอื่น ๆ เลย การยุ่งกับส่วนอื่น ๆ มีความเสี่ยงที่จะทำให้พังทั้งระบบได้
+
 
 
 */
